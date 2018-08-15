600f82bc47dd36260c1d8acfb2bc81905b1693b2
Abigail Demsas <ademsas1026@gmail.com>
2018-08-15 12:10:17 -0400

    title overlays styled

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,9 +4,7 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 import { markerIcon, loadingIcon, errorIcon } from '../mapElements'
 import { chooseCluster } from '../data/utils'
-// const { mostCommonWords, allDataCommonWords } = require('../../script/prepData')
 
-// add click handler that makes use of this.props.dbRef
 class MapView extends Component {
   constructor() {
     super()
@@ -31,9 +29,7 @@ class MapView extends Component {
   }
   
   render() {
-    // console.log('this.state: ', this.state, 'map props: ', this.props)
     const { center, zoom, latitude, longitude, sightings } = this.state
-    console.log('this is the cluster of sightings on state: ', typeof sightings)
     return (
       <div id="mapid">
         <Map 
@@ -43,10 +39,10 @@ class MapView extends Component {
           onclick={this.loadCluster}
           id="actualMap"
         >
-          <h1 style={{zIndex: '1000', color: '#fff'}}>Not Alone</h1>
-          <h2 style={{zIndex: '1000', color: '#fff'}}>An Interactive Visualization of UFO Sightings in the US</h2>
-          <h3 style={{zIndex: '1000', color: '#fff'}}>1949 - 2013</h3>
-          <h3 style={{zIndex: '1000', color: '#fff'}}>click map...if you dare</h3>
+          <Typography variant="display1" style={{zIndex: '1000', color: '#fff' }}>Not Alone</Typography>
+          <Typography variant="subheading" style={{zIndex: '1000', color: '#fff', position: 'relative', left: '2.5vw', top: '1vh'}}>An Interactive Visualization of UFO Sightings in the US</Typography>
+          <Typography variant="subheading" style={{zIndex: '1000', color: '#fff', position: 'relative', left: '2.5vw', top: '2vh'}}>1949 - 2013</Typography>
+          <Typography variant="subheading" style={{zIndex: '1000', color: '#fff', position: 'relative', left: '2.5vw', top: '4vh'}}>click map...if you dare</Typography>
           <TileLayer  
             url="https://api.mapbox.com/styles/v1/ademsas/cjggt8ilb002k2rqw269apfqt/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRlbXNhcyIsImEiOiJjamdncThncmIwMGw4MnhxbWNybnV1cDMwIn0.DmUIWxfIPjHyD-nu9GVqrw"
             attribution="data courtesy of the National UFO Reporting Center (NUFORC)"
